3a382da23432e87f0fa71dad9e04f60f7ffcdfc1
Mohammad.Ansari1 <mohammad.ansari1@timesinternet.in>
2017-09-26 14:49:27 +0530

    heap

--- src/main/java/com/kha/ds/GFG.java
+++ src/main/java/com/kha/ds/GFG.java
@@ -5,93 +5,78 @@ package main.java.com.kha.ds;
  */
 public class GFG {
 
-    public static void swap(int[] arr1, int[] arr2, int[] arr3, int a, int b){
-        int temp = arr1[a];
-        arr1[a] = arr1[b];
-        arr1[b]=temp;
 
-        temp = arr2[a];
-        arr2[a] = arr2[b];
-        arr2[b]=temp;
 
-        temp = arr3[a];
-        arr3[a] = arr3[b];
-        arr3[b]=temp;
-
-    }
-
-    public static void heapify(int[] a, int []b, int []c){
-        int n = a.length;
-        int pIndex = 0;
-        int leftIndex = 0;
-        int rightIndex = 0;
-        while(pIndex < n){
-            leftIndex = 2*pIndex+1;
-            rightIndex = 2*pIndex+2;
-
-            if(leftIndex < n && rightIndex <n ){
-                if(a[pIndex] > a[leftIndex] && a[leftIndex] < a[rightIndex]){
-                    swap(a, b, c, pIndex, leftIndex);
-                    pIndex = leftIndex;
-                }else if (a[pIndex] > a[rightIndex] && a[rightIndex] < a[leftIndex]){
-                    swap(a, b, c, pIndex, rightIndex);
-                    pIndex = rightIndex;
-                }else {
-                    break;
-                }
-
-            }else if(leftIndex < n){
-                if(a[pIndex] > a[leftIndex]){
-                    swap(a, b, c, pIndex, leftIndex);
-                    pIndex = leftIndex;
-                }else {
-                    break;
-                }
-
-            }else if(rightIndex < n){
-                if(a[pIndex] > a[rightIndex]){
-                    swap(a, b, c, pIndex, rightIndex);
-                    pIndex = rightIndex;
-                }else {
-                    break;
-                }
-            }else {
-                System.out.print(" ");
-                break;
-            }
-        }
-        System.out.print(" ");
+    private static void swap(int[] heap, int n1, int n2){
+        int temp = heap[n1];
+        heap[n1] = heap[n2];
+        heap[n2] = temp;
     }
 
 
     public static void kthSmallest(int[][] a,int n,int k){
         int[] heap = new int[n];
-        int[] col = new int[n];
         int[] row = new int[n];
+        int[] col = new int[n];
+
         for(int i=0;i<n;i++){
             heap[i] = a[0][i];
-            col[i] = i;
-            row[i] = 0;
+            col[i]=i;
         }
 
-
-        for(int i=0;i<k-1;i++){
-            row[0] = row[0] + 1;
-            //System.out.print(heap[0]);
-            if(row[0]<n){
-                heap[0] =  a[row[0]][col[0]];
-            }else{
-                heap[0] = Integer.MAX_VALUE;
+        for(int i=0;i<k;i++){
+            System.out.println(i +" : "+ heap[0]);
+            if(i==k-1){
+                System.out.println(heap[0]);
             }
-            heapify(heap, col, row);
-            System.out.print("");
-        }
-        System.out.print(heap[0]);
+            int nextCol = col[0];
+            if(row[0] < n-1){
+                int nextRow = ++row[0];
+                heap[0] = a[nextRow][nextCol];
+            }else {
+                heap[0] = 200;
+            }
+            int pIndex = 0;
+            int lIndex = 2 *pIndex+1;
+            int rIndex = 2 *pIndex+2;
 
+            while( lIndex < n || rIndex < n){
+                if(lIndex < n && rIndex < n && heap[lIndex]<heap[pIndex] && heap[lIndex] < heap[rIndex]){
+                    swap(heap, lIndex, pIndex);
+                    swap(row , lIndex, pIndex);
+                    swap(col, lIndex,pIndex);
+                    pIndex = lIndex;
+                }else if(lIndex < n && rIndex < n && heap[rIndex]<heap[pIndex] && heap[rIndex] < heap[lIndex]){
+                    swap(heap, rIndex, pIndex);
+                    swap(row , rIndex, pIndex);
+                    swap(col, rIndex,pIndex);
+                    pIndex = rIndex;
+                }else if(lIndex < n && heap[lIndex]<heap[pIndex]){
+                    swap(heap, lIndex, pIndex);
+                    swap(row , lIndex, pIndex);
+                    swap(col, lIndex,pIndex);
+                    pIndex = lIndex;
+                }else if(rIndex < n && heap[rIndex]<heap[pIndex]){
+                    swap(heap, rIndex, pIndex);
+                    swap(row , rIndex, pIndex);
+                    swap(col, rIndex,pIndex);
+                    pIndex = rIndex;
+                }else {
+                    break;
+                }
+                 lIndex = 2 *pIndex+1;
+                 rIndex = 2 *pIndex+2;
+            }
+
+        }
     }
+
+
 
     public static void main(String[] args) {
         int n=4;
+        //String str = "1 3 9 20 20 25 29 30 33 56 65 69 83 2 6 14 23 26 28 30 37 47 58 68 72 84 2 7 18 24 30 31 36 37 52 65 71 76 90 3 12 22 25 30 37 38 50 57 66 74 85 92 4 13 22 27 33 42 44 51 57 66 75 85 94 4 15 24 29 35 42 45 52 60 68 80 87 94 6 15 25 29 38 43 47 53 63 69 81 88 96 8 16 25 30 40 46 55 59 68 70 81 88 97 9 18 27 32 40 51 57 61 69 74 82 89 97 10 19 28 33 41 52 63 68 71 77 83 89 98 12 20 28 35 41 57 65 71 76 79 85 92 98 14 20 30 44 46 66 68 72 77 82 87 96 99 16 22 35 54 57 68 71 79 87 93 95 98 100 ";
+        //String[] st = str.split(" ");
         int[] input = new int[]{16,28,60,64,22,41,63,91,27,50,87,93,36,78,87,94};
         int[][] arr = new int[n][n];
         int index = 0;
@@ -100,6 +85,6 @@ public class GFG {
                 arr[i][j]=input[index++];
             }
         }
-        kthSmallest(arr, n, 3);
+        kthSmallest(arr, n, 16);
     }
 }
